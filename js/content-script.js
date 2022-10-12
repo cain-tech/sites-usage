81c3ae58214af62d66be53c670eeb50b6ee21927
cain-tech <89671114+cain-tech@users.noreply.github.com>
2022-10-12 14:58:02 +0300

    Add files via upload

--- js/content-script.js
+++ js/content-script.js
@@ -8,11 +8,8 @@ const updateRating = () => {
 
 
 const updateUI = () => chrome.storage.local.get([RATING_STORAGE_KEY], ({ rating }) => {
-    if(!rating || !rating.length) return
-
-    const currentRating = rating.find(({ host })=> location.host === host)
-
-    if (!currentRating) return;
+    let currentRating
+    if(rating && rating.length) currentRating = rating.find(({ host })=> location.host === host)
 
     const floatingRate = document.getElementById('site-usage-rating')
 
@@ -24,14 +21,19 @@ const updateUI = () => chrome.storage.local.get([RATING_STORAGE_KEY], ({ rating 
         height:80px;
         bottom:40px;
         right:40px;
-        background-color: rgba(255, 0, 0, ${currentRating.rating / 10});
+        background-color: ${currentRating ? `rgba(255, 0, 0, ${currentRating.rating / 10})` : 'green'};
         color:black;
         border-radius:50px;
         text-align:center;
         z-index: 9999;
         font-size: 13px;
-        padding: 5px;
-    ">Site Malicious Rate<br><b>${currentRating.rating}</b></div>`
+        padding: 6px;
+        font-family: monospace;
+        line-height: 15px;
+        word-wrap: normal;
+    ">
+        Site Malicious Rate<br><b>${currentRating ? currentRating.rating : '0.0'}</b>    
+    </div>`
 })
 
 updateRating()
